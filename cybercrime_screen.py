d970978df099954c49318f8704004afe1caa34c3
CyberSirennn <tscottvaughn@gmail.com>
2025-02-05 14:38:39 +0000

    new branch and .py file created for api assignment

---
+++ cybercrime_screen.py
@@ -0,0 +1,11 @@
+# This console application is designed to interact with the UK Police API to retrieve cyber-related crime trends in major cities.
+
+import requests
+# API endpoint for fetching crime categories
+URL = "https://data.police.uk/docs/"
+response = responses.get("https://data.police.uk/docs/")
+
+
+# Fetch crime data
+
+
