fbfb3ae5348e09f7f560fe878ad975cc94e6a2b4
CyberSirennn <tscottvaughn@gmail.com>
2025-02-09 22:10:36 +0000

    final tweaks to code

--- criminalthreatchecker.py
+++ criminalthreatchecker.py
@@ -1,22 +1,22 @@
 from fileinput import filename
 
-import requests
-import json
+import requests # for API requests
+import json # for saving results to a JSON file
 
 print("\nWelcome to the Criminal Threat Checker (CTC).\nThis console application is designed to interact with the UK Police API to retrieve crime trends in major cities.")
 
-# Step 1: fetch all available UK police forces
+# Step 1: fetch all available police forces from UK open police API
 def get_police_forces():
     """Fetches and prints all available UK Police Forces."""
-    url = "https://data.police.uk/api/forces" # API endpoint for fetching crime categories
+    url = "https://data.police.uk/api/forces" # API endpoint for fetching all available police forces in the UK.
     response = requests.get(url)
 
     if response.status_code == 200:
-        forces = response.json()
+        forces = response.json() # data retrieved from the API is in JSON format
         print("\nAvailable Police Forces: ")
         for force in forces:
             print(f"{force['id']}: {force['name']}")
-        return {force['id']: force['name'] for force in forces} # to list all UK police forces
+        return {force['id']: force['name'] for force in forces} # to return list all UK police forces
     else:
         print("Error: " .format(response.status_code, response.text))
         return {}
@@ -24,7 +24,7 @@ def get_police_forces():
 # Step 2: Fetch crimes for a chosen police force by city
 def fetch_crimes_by_force(force_id):
     """Fetches crimes for specific UK police force."""
-    force_id_slice = force_id[:5] # to slice the first 5 characters of the force ID
+    force_id_slice = force_id[:5] # to slice the first 5 characters of the force ID in program output
     url = f"https://data.police.uk/api/crimes-no-location?category=all-crime&force={force_id}"
     response = requests.get(url)
 
@@ -40,7 +40,7 @@ def save_to_json(data, filename):
     """Saves data to a JSON file in readable format."""
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
-    print(f"\n✅Great! Your crime data successfully saved to {filename}")
+    print(f"\n✅Great! Your crime data successfully saved to {filename}") # once data is filtered and processed accordingly, it's saved to a file in a readable format to be used or analysed further.
 
 # Step 4: Main program
 def main():
